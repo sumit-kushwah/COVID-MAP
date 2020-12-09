56ac400832e9d3ccfcb767a7c517b32ce4e5b0e2
sumit-kushwah <sumitkushwah1729@gmail.com>
2020-12-10 02:42:17 +0530

    removed material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,12 @@ import { HomeModule } from './home/home.module';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
+import { A11yModule } from '@angular/cdk/a11y';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,6 +25,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HomeModule,
     FormsModule,
     BrowserAnimationsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/indiamap/indiamap.module.ts
+++ src/app/indiamap/indiamap.module.ts
@@ -1,20 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndiamapComponent } from './indiamap.component';
-
-import { A11yModule } from '@angular/cdk/a11y';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-
 @NgModule({
   declarations: [IndiamapComponent],
   imports: [
-    A11yModule,
     CommonModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatInputModule,
   ],
   exports: [IndiamapComponent]
 })
