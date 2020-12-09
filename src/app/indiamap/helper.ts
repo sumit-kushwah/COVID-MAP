18bf9e0d3473095f32b5e4f00b7191af76ef7084
sumit-kushwah <sumitkushwah1729@gmail.com>
2020-12-10 02:23:00 +0530

    angular material added

--- src/app/indiamap/helper.ts
+++ src/app/indiamap/helper.ts
@@ -1,3 +1,100 @@
-export function plotter(el: any) {
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4maps from "@amcharts/amcharts4/maps";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4geodata_indiaLow from "@amcharts/amcharts4-geodata/indiaLow";
+import am4themes_dark from "@amcharts/amcharts4/themes/dark";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+
+am4core.useTheme(am4themes_dark);
+am4core.useTheme(am4themes_animated);
+
+export function mapPlotter(el: HTMLElement, data: { id: string, value: number }[]) {
+  // Themes end
+
+  // Create map instance
+  let chart = am4core.create(el, am4maps.MapChart);
+
+  // Set map definition
+  chart.geodata = am4geodata_indiaLow;
+
+  // Set projection
+  // chart.projection = new am4maps.projections.;
+
+  // Create map polygon series
+  let polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+
+  //Set min/max fill color for each area
+  polygonSeries.heatRules.push({
+    property: "fill",
+    target: polygonSeries.mapPolygons.template,
+    min: chart.colors.getIndex(1).brighten(1),
+    max: chart.colors.getIndex(1).brighten(-0.3)
+  });
+
+  // Make map load polygon data (state shapes and names) from GeoJSON
+  polygonSeries.useGeodata = true;
+
+  // Set heatmap values for each state
+  polygonSeries.data = data;
+
+  // Configure series tooltip
+  let polygonTemplate = polygonSeries.mapPolygons.template;
+  polygonTemplate.tooltipText = "{name}: {value}";
+  polygonTemplate.nonScalingStroke = true;
+  polygonTemplate.strokeWidth = 0.5;
+
+  // Create hover state and set alternative fill color
+  let hs = polygonTemplate.states.create("hover");
+  hs.properties.fill = am4core.color("#3c5bdc");
+
+}
+
+export function barPlotter(el: HTMLElement, data: any) {
+  let chart = am4core.create(el, am4charts.XYChart);
+  chart.scrollbarX = new am4core.Scrollbar();
+
+  // Add data
+  chart.data = data.slice(0, 10);
+
+  // Create axes
+  let categoryAxis: any = chart.xAxes.push(new am4charts.CategoryAxis());
+  categoryAxis.dataFields.category = "name";
+  categoryAxis.renderer.grid.template.location = 0;
+  categoryAxis.renderer.minGridDistance = 30;
+  categoryAxis.renderer.labels.template.horizontalCenter = "right";
+  categoryAxis.renderer.labels.template.verticalCenter = "middle";
+  categoryAxis.renderer.labels.template.rotation = 270;
+  categoryAxis.tooltip.disabled = true;
+  categoryAxis.renderer.minHeight = 110;
+
+  let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+  valueAxis.renderer.minWidth = 50;
+
+  // Create series
+  let series: any = chart.series.push(new am4charts.ColumnSeries());
+  series.sequencedInterpolation = true;
+  series.dataFields.valueY = "value";
+  series.dataFields.categoryX = "name";
+  series.tooltipText = "[{categoryX}: bold]{valueY}[/]";
+  series.columns.template.strokeWidth = 0;
+
+  series.tooltip.pointerOrientation = "vertical";
+
+  series.columns.template.column.cornerRadiusTopLeft = 10;
+  series.columns.template.column.cornerRadiusTopRight = 10;
+  series.columns.template.column.fillOpacity = 0.8;
+
+  // on hover, make corner radiuses bigger
+  let hoverState = series.columns.template.column.states.create("hover");
+  hoverState.properties.cornerRadiusTopLeft = 0;
+  hoverState.properties.cornerRadiusTopRight = 0;
+  hoverState.properties.fillOpacity = 1;
+
+  series.columns.template.adapter.add("fill", function(fill: any, target: { dataItem: { index: number; }; }) {
+    return chart.colors.getIndex(target.dataItem.index);
+  });
+
+  // Cursor
+  chart.cursor = new am4charts.XYCursor();
 
 }
