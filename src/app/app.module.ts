c635c1ea92dd7b2732a01ad7a32ba6b1fe062655
sumit-kushwah <sumitkushwah1729@gmail.com>
2020-12-10 02:23:13 +0530

    forms import added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IndiaModule } from './india/india.module';
-import { WorldModule } from './world/world.module';
 import { HttpClientModule } from '@angular/common/http';
-import { IndiamapComponent } from './indiamap/indiamap.component';
 import { HomeModule } from './home/home.module';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,6 +17,8 @@ import { HomeModule } from './home/home.module';
     AppRoutingModule,
     HttpClientModule,
     HomeModule,
+    FormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
