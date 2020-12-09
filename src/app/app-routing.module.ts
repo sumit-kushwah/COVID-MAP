fe50e8408385f8406d1733b343d5df3a5d1575c8
sumit-kushwah <sumitkushwah1729@gmail.com>
2020-12-10 02:56:01 +0530

    minor fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PageNotFoundComponent } from 'src/page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
 import { IndiamapComponent } from './indiamap/indiamap.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { WorldmapComponent } from './worldmap/worldmap.component';
 
 const routes: Routes = [
