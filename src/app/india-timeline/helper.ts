0d362e058ae5a7d7337da7bad36d44457295fc98
sumit-kushwah <sumitkushwah1729@gmail.com>
2020-12-10 16:29:38 +0530

    india timeline implemented

---
+++ src/app/india-timeline/helper.ts
@@ -0,0 +1,72 @@
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_dark from "@amcharts/amcharts4/themes/dark";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+
+/* Chart code */
+// Themes begin
+am4core.useTheme(am4themes_dark);
+am4core.useTheme(am4themes_animated);
+am4core.options.autoDispose = true;
+// Themes end
+
+export function plotter(el: HTMLElement, data: any) {
+  let chart: any = am4core.create(el, am4charts.XYChart);
+
+  // Add data
+  chart.data = data;
+
+  // Set input format for the dates
+  chart.dateFormatter.inputDateFormat = "yyyy-MM-dd";
+
+  // Create axes
+  let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+  let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+  // Create series
+  let series: any = chart.series.push(new am4charts.LineSeries());
+  series.dataFields.valueY = "value";
+  series.dataFields.dateX = "date";
+  series.tooltipText = "{value}"
+  series.strokeWidth = 2;
+  series.minBulletDistance = 15;
+
+  // Drop-shaped tooltips
+  series.tooltip.background.cornerRadius = 20;
+  series.tooltip.background.strokeOpacity = 0;
+  series.tooltip.pointerOrientation = "vertical";
+  series.tooltip.label.minWidth = 40;
+  series.tooltip.label.minHeight = 40;
+  series.tooltip.label.textAlign = "middle";
+  series.tooltip.label.textValign = "middle";
+
+  // Make bullets grow on hover
+  let bullet = series.bullets.push(new am4charts.CircleBullet());
+  bullet.circle.strokeWidth = 2;
+  bullet.circle.radius = 4;
+  bullet.circle.fill = am4core.color("#fff");
+
+  let bullethover = bullet.states.create("hover");
+  bullethover.properties.scale = 1.3;
+
+  // Make a panning cursor
+  chart.cursor = new am4charts.XYCursor();
+  chart.cursor.behavior = "panXY";
+  chart.cursor.xAxis = dateAxis;
+  chart.cursor.snapToSeries = series;
+
+  // Create vertical scrollbar and place it before the value axis
+  chart.scrollbarY = new am4core.Scrollbar();
+  chart.scrollbarY.parent = chart.leftAxesContainer;
+  chart.scrollbarY.toBack();
+
+  // Create a horizontal scrollbar with previe and place it underneath the date axis
+  chart.scrollbarX = new am4charts.XYChartScrollbar();
+  chart.scrollbarX.series.push(series);
+  chart.scrollbarX.parent = chart.bottomAxesContainer;
+
+  dateAxis.start = 0.79;
+  dateAxis.keepSelection = true;
+
+  return chart;
+}
--- src/app/india-timeline/india-timeline.component.ts
+++ src/app/india-timeline/india-timeline.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { tap } from 'rxjs/internal/operators/tap';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { ApiService } from '../api.service';
+import { plotter } from './helper';
 
 @Component({
   selector: 'app-india-timeline',
@@ -7,9 +11,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndiaTimelineComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
+
+  subscriptions: Subscription[] = [];
+
+  chartRefDict:any = [];
+
+  plotdata:{ date: string; value: number }[] = [];
+
+  @ViewChild('timeline', { static: true })
+  timeline!: ElementRef;
 
   ngOnInit(): void {
+    this.subscriptions.push(this.apiService.indiaTimeline().pipe(
+      tap((data) => { this.apiService.india_timeline_data = data; }),
+      tap(() => this.setPlotForKey('dailyconfirmed')),
+    ).subscribe(() => {}, error => console.log(error)));
   }
 
+  setPlotForKey(key: string) {
+    let list:any = this.apiService.india_timeline_data;
+    // data for chart
+    this.plotdata = [];
+    list.forEach((item: any) => {
+      this.plotdata.push({
+        date: item.dateymd,
+        value: item[key]
+      })
+    })
+    this.chartDestroy();
+    this.chartRefDict.push(plotter(this.timeline.nativeElement, this.plotdata));
+  }
+
+  onSelect(event: any) {
+    let key = event.target.value;
+    console.log(key);
+    this.setPlotForKey(key);
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+    this.chartDestroy();
+  }
+
+  chartDestroy() {
+    this.chartRefDict.forEach((chart:any) => { if (chart) { chart.dispose(); } });
+    this.chartRefDict = [];
+  }
 }
